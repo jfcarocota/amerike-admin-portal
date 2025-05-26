91327159616be5d152081b74731d8bd10828cbb6
jcaro <jfcarocota@gmail.com>
2025-05-26 13:56:49 -0700

    edit style view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import CharacterView from './components/CharacterView/CharacterView'
 import CharactersDataListView from './components/CharactersListView/CharactersListView'
 import StyleNameDataListView from "./components/StyleNameDataListView/StyleNameDataListView.jsx";
+import StyleNameDataView from "./components/StyleNameDataView/StyleNameDataView.jsx";
 
 const App = ()=> {
 
@@ -10,7 +11,7 @@ const App = ()=> {
         <Route index element={<CharactersDataListView/>}/>
         <Route path='/styles/' element={<StyleNameDataListView/>}/>
         <Route path='character/:id' element={<CharacterView/>}/>
-        <Route path='/styles/:id' element={<StyleNameDataListView/>}/>
+        <Route path='/styles/:id' element={<StyleNameDataView/>}/>
     </Routes>
   )
 }
--- src/components/StyleNameDataView/StyleNameDataView.jsx
+++ src/components/StyleNameDataView/StyleNameDataView.jsx
@@ -1,24 +1,70 @@
 import {useParams} from "react-router";
+import {gql, useMutation, useQuery} from "@apollo/client";
+import './StyleNameDataView.css';
+import {useState} from "react";
+
+const styleNameDataById = gql`
+query StyleNameDataById($id: String!) {
+  StyleNameDataById(id: $id) {
+    id
+    styleName
+    priority
+  }
+}`
+;
+
+const EditStyleData = gql`
+  mutation EditStyleNameData($id: String!, $style: String!, $stylePriority: Int!) {
+  editStyleNameData(id: $id, styleName: $style, priority: $stylePriority) {
+    id
+    styleName
+    priority
+  }
+}
+`;
+
 
 const StyleNameDataView = ()=> {
     const {id} = useParams();
+    const [stylePriority, setStylePriority] = useState(0);
+    const [style, setStyle] = useState("");
+
+    const {data, loading, error} = useQuery(styleNameDataById, {
+        variables: {id}
+    });
+
+    const [editStyleData] = useMutation(EditStyleData, {
+        variables: {
+            id,
+            stylePriority,
+            style,
+        }
+    });
+
+    if(loading) return <div>Loading style data...</div>
+    if(error) return <div>Error :c {error.message}</div>
+
+    const {styleName, priority} = data.StyleNameDataById;
+
+    const handlePriorityChange = event => {
+        setStylePriority(Number(event.target.value));
+        setStyle(styleName);
+    }
+
 
     return (
         <>
-            <h1>Character Editor Panel</h1>
-            {
-                data.CharacterDataList.map(characterData => {
-                    return (
-                        <div key={characterData.id}>
-                            <h2>Player: {characterData.styleName.styleName} Priority: {characterData.styleName.priority}</h2>
-
-                            <div>
-                                <button onClick={()=> showEditView(characterData.id)}>Edit</button>
-                            </div>
-                        </div>
-                    );
-                })
-            }
+            <h1>Style priority Editor Panel</h1>
+            <div>
+                <h1>Style: {styleName}</h1>
+                <div className='field'>
+                    <div>Priority:</div>
+                    <input onChange={handlePriorityChange} placeholder='Priority' type='number' defaultValue={priority}/>
+                </div>
+            </div>
+            <div>
+                <button onClick={editStyleData}>submit</button>
+            </div>
         </>
     );
 }
